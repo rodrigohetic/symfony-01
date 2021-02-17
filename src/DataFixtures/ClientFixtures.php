629a8e4ce43d75c1a7aaaf621dcf524f732e4a47
rodrigohetic <rodrigo.tapia-bravo@hetic.net>
2021-02-17 10:04:01 +0100

    fix(client): fixtures

--- src/DataFixtures/ClientFixtures.php
+++ src/DataFixtures/ClientFixtures.php
@@ -13,7 +13,7 @@ class ClientFixtures extends AppFixtures
             return (new Client())
                 ->setEmail($this->faker->email())
                 ->setAge($this->faker->numberBetween(18,99))
-                ->setWeight($this->faker->randomFloat(null,0,null))
+                ->setWeight($this->faker->randomFloat(0,0,50))
                 ->setName($this->faker->firstName())
                 ->setNumberBeer($this->faker->numberBetween(6,99));
         });
