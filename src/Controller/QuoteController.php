1c1e2b1863176f6330d46675b4f5588c6fdb763e
rodrigohetic <rodrigo.tapia-bravo@hetic.net>
2021-03-24 10:15:16 +0100

    feat(form) : add new form quote

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -3,11 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Quote;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Form\QuoteType;
 use App\Repository\QuoteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -34,11 +32,8 @@ class QuoteController extends AbstractController
     {
         $quote = new Quote();
 
-        $form = $this->createFormBuilder($quote)
-            ->add('title', TextType::class, ['required' => true, 'label' => 'Titre de la citation '])
-            ->add('content', TextareaType::class, ['required' => true, 'label' => 'Markdown '])
-            ->add('save', SubmitType::class, ['label' => 'Create Quote'])
-            ->getForm();
+        $form = $this->createForm(QuoteType::class, $quote);
+
 
         $form->handleRequest($request);
 
---
+++ src/Form/QuoteType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Quote;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class QuoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Quote'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Quote::class,
+        ]);
+    }
+}
